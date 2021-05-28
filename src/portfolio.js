6aeedd8d8a1095338fe3295000c0cc2eb1ab18c3
Callum <Callump01@users.noreply.github.com>
2021-05-28 22:20:18 +0100

    Update portfolio.js
    Initial upload

--- src/portfolio.js
+++ src/portfolio.js
@@ -7,80 +7,80 @@ const settings = {
 
 //SEO Related settings
 const seo = {
-  title: "Ashutosh's Portfolio",
+  title: "Callum Pope",
   description:
-    "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
+    "A Confident, reliable and multi-skilled consultant with a comprehensive understanding and experience in designing, developing, and deploying Identity and Access Management (IAM) infrastructure.",
   og: {
-    title: "Ashutosh Hathidara Portfolio",
+    title: "Callum Pope",
     type: "website",
-    url: "http://ashutoshhathidara.com/",
+    url: "http://callumpope.com/",
   },
 };
 
 //Home Page
 const greeting = {
-  title: "Ashutosh Hathidara",
-  logo_name: "AshutoshHathidara",
-  nickname: "layman_brother",
+  title: "Callum Pope",
+  logo_name: "CallumPope",
+  nickname: "Snr. Identity & Access Management Specialist",
   subTitle:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   resumeLink:
-    "https://drive.google.com/open?id=1XYpYhLeqCdyx_q6l0bQoC7RgwQjAjXPf",
-  portfolio_repository: "https://github.com/ashutosh1919/masterPortfolio",
+    "",
+  portfolio_repository: "https://github.com/Callump01/masterPortfolio",
 };
 
 const socialMediaLinks = [
   /* Your Social Media Link */
   // github: "https://github.com/ashutosh1919",
-  // linkedin: "https://www.linkedin.com/in/ashutosh-hathidara-88710b138/",
+  // linkedin: "https://www.linkedin.com/in/callumpope/",
   // gmail: "ashutoshhathidara98@gmail.com",
   // gitlab: "https://gitlab.com/ashutoshhathidara98",
   // facebook: "https://www.facebook.com/laymanbrother.19/",
   // twitter: "https://twitter.com/ashutosh_1919",
   // instagram: "https://www.instagram.com/layman_brother/"
 
-  {
-    name: "Github",
-    link: "https://github.com/ashutosh1919",
-    fontAwesomeIcon: "fa-github", // Reference https://fontawesome.com/icons/github?style=brands
-    backgroundColor: "#181717", // Reference https://simpleicons.org/?q=github
-  },
+ //{
+ //   name: "Github",
+ //   link: "https://github.com/ashutosh1919",
+ //   fontAwesomeIcon: "fa-github", // Reference https://fontawesome.com/icons/github?style=brands
+ //   backgroundColor: "#181717", // Reference https://simpleicons.org/?q=github
+ // },
   {
     name: "LinkedIn",
-    link: "https://www.linkedin.com/in/ashutosh-hathidara-88710b138/",
+    link: "https://www.linkedin.com/in/callumpope/",
     fontAwesomeIcon: "fa-linkedin-in", // Reference https://fontawesome.com/icons/linkedin-in?style=brands
     backgroundColor: "#0077B5", // Reference https://simpleicons.org/?q=linkedin
   },
-  {
-    name: "YouTube",
-    link: "https://www.youtube.com/channel/UC_amoXmmxSY9KusoDczDTXQ",
-    fontAwesomeIcon: "fa-youtube", // Reference https://fontawesome.com/icons/youtube?style=brands
-    backgroundColor: "#FF0000", // Reference https://simpleicons.org/?q=youtube
-  },
+  //{
+  //  name: "YouTube",
+  //  link: "https://www.youtube.com/channel/UC_amoXmmxSY9KusoDczDTXQ",
+  //  fontAwesomeIcon: "fa-youtube", // Reference https://fontawesome.com/icons/youtube?style=brands
+  //  backgroundColor: "#FF0000", // Reference https://simpleicons.org/?q=youtube
+  //},
   {
     name: "Gmail",
-    link: "mailto:ashutoshhathidara98@gmail.com",
+    link: "mailto:contact@callumpope.com",
     fontAwesomeIcon: "fa-google", // Reference https://fontawesome.com/icons/google?style=brands
     backgroundColor: "#D14836", // Reference https://simpleicons.org/?q=gmail
   },
-  {
-    name: "Twitter",
-    link: "https://twitter.com/ashutosh_1919",
-    fontAwesomeIcon: "fa-twitter", // Reference https://fontawesome.com/icons/twitter?style=brands
-    backgroundColor: "#1DA1F2", // Reference https://simpleicons.org/?q=twitter
-  },
-  {
-    name: "Facebook",
-    link: "https://www.facebook.com/laymanbrother.19/",
-    fontAwesomeIcon: "fa-facebook-f", // Reference https://fontawesome.com/icons/facebook-f?style=brands
-    backgroundColor: "#1877F2", // Reference https://simpleicons.org/?q=facebook
-  },
-  {
-    name: "Instagram",
-    link: "https://www.instagram.com/layman_brother/",
-    fontAwesomeIcon: "fa-instagram", // Reference https://fontawesome.com/icons/instagram?style=brands
-    backgroundColor: "#E4405F", // Reference https://simpleicons.org/?q=instagram
-  },
+  //{
+  //  name: "Twitter",
+  //  link: "https://twitter.com/ashutosh_1919",
+  //  fontAwesomeIcon: "fa-twitter", // Reference https://fontawesome.com/icons/twitter?style=brands
+  //  backgroundColor: "#1DA1F2", // Reference https://simpleicons.org/?q=twitter
+  //},
+  //{
+  //  name: "Facebook",
+  //  link: "https://www.facebook.com/laymanbrother.19/",
+  //  fontAwesomeIcon: "fa-facebook-f", // Reference https://fontawesome.com/icons/facebook-f?style=brands
+  //  backgroundColor: "#1877F2", // Reference https://simpleicons.org/?q=facebook
+  //},
+  //{
+  //  name: "Instagram",
+  //  link: "https://www.instagram.com/layman_brother/",
+  //  fontAwesomeIcon: "fa-instagram", // Reference https://fontawesome.com/icons/instagram?style=brands
+  //  backgroundColor: "#E4405F", // Reference https://simpleicons.org/?q=instagram
+  //},
 ];
 
 const skills = {
